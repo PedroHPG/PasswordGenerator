2b6a1a82f6bca5461963bd906b60fb5e308fba00
PedroHPG <pedrohpgoncalves@hotmail.com>
2026-01-12 10:04:28 -0300

    Outlined program plans

---
+++ passgen.js
@@ -0,0 +1,17 @@
+//Asks user for desired length
+
+//Ask user if they want lowercase
+
+//Ask user if they want uppercase
+
+//Ask user if they want numbers
+
+//Ask user if they want special characters
+
+//Merge character sets and create password variable
+
+//Loop for desired length
+
+//Generate secure random value and add to password
+
+//return generated password
