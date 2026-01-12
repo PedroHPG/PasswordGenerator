9d602794969dfb795e04a5da071335980f8ff498
PedroHPG <pedrohpgoncalves@hotmail.com>
2026-01-12 10:01:17 -0300

    Removed existing tests

--- passgen.test.js
+++
@@ -1,7 +0,0 @@
-import { passgen } from './passgen';
-
-describe('passgen', () => {
-    it('returns "Invalid input" if length passed is 0', () => {
-        expect(passgen(0)).toBe("Invalid input");
-    })
-})
