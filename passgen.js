3106dd274e60fb37865a10b5cf71ea2db6304bb7
PedroHPG <pedrohpgoncalves@hotmail.com>
2026-01-12 10:16:13 -0300

    Prompt user for desired settings

--- passgen.js
+++ passgen.js
@@ -1,16 +1,15 @@
 const prompt = require('prompt-sync')();
 
 //Asks user for desired length
+let length = prompt("How many characters should your password be? Recommended: 8 or more. Enter number here: ");
 
-//Ask user if they want lowercase
+//Ask user if they want lowercase, uppercase, numbers and/or special characters
+let lowercase = prompt("Would you like to use lowercase letters? Enter 'n' for no, anything else for yes. Enter here: ")
+let uppercase = prompt("Would you like to use uppercase letters? Enter 'n' for no, anything else for yes. Enter here: ")
+let numbers = prompt("Would you like to use numbers? Enter 'n' for no, anything else for yes. Enter here: ")
+let special = prompt("Would you like to use special characters? Enter 'n' for no, anything else for yes. Enter here: ")
 
-//Ask user if they want uppercase
-
-//Ask user if they want numbers
-
-//Ask user if they want special characters
-
-//Merge character sets and create password variable
+//Prepare character sets and create password variable
 
 //Loop for desired length
 
