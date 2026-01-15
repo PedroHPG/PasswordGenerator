8ee78cf5ef98b7a05d049b0072814bb2b7d10a87
PedroHPG <pedrohpgoncalves@hotmail.com>
2026-01-15 10:27:30 -0300

    Reformatted code into smaller functions

--- passgen.js
+++ passgen.js
@@ -1,45 +1,52 @@
 const crypto = require('node:crypto');
 const prompt = require('prompt-sync')();
+
 const lowerCharSet = "abcdefghijklmnopqrstuvwxyz";
 const upperCharSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numCharSet = "1234567890";
 const specialCharSet = "!@#$%^&*()_+=-";
 
-//Asks user for desired length
+let passwordCharSet = "";
+
+function prepareCharSet(){
+    let lowercase = prompt("Would you like to use lowercase letters? Enter 'n' for no, anything else for yes. Enter here: ")
+    let uppercase = prompt("Would you like to use uppercase letters? Enter 'n' for no, anything else for yes. Enter here: ")
+    let numbers = prompt("Would you like to use numbers? Enter 'n' for no, anything else for yes. Enter here: ")
+    let special = prompt("Would you like to use special characters? Enter 'n' for no, anything else for yes. Enter here: ")
+
+    if (lowercase != 'n'){
+        passwordCharSet = passwordCharSet.concat(lowerCharSet);
+    }
+    if (uppercase != 'n'){
+        passwordCharSet = passwordCharSet.concat(upperCharSet);
+    }
+    if (numbers != 'n'){
+        passwordCharSet = passwordCharSet.concat(numCharSet);
+    }
+    if (special != 'n'){
+        passwordCharSet = passwordCharSet.concat(specialCharSet);
+    }
+
+    console.log(`Possible characters for your password were: ${passwordCharSet}`);
+}
+
+function createPassword(length){
+    let password = "";
+    let nextChar = "";
+    let randNum = 0;
+    for(let i = 0; i < length; i++){
+        //Generate secure random value and add to password
+        randNum = crypto.randomInt(passwordCharSet.length);
+        nextChar = passwordCharSet.charAt(randNum);
+        password = password.concat(nextChar);
+    }
+
+    console.log(`Your new password is: ${password}. Keep it somewhere safe!`);
+}
+
+prepareCharSet();
+
 let length = prompt("How many characters should your password be? Recommended: 8 or more. Enter number here: ");
 
-//Ask user if they want lowercase, uppercase, numbers and/or special characters
-let lowercase = prompt("Would you like to use lowercase letters? Enter 'n' for no, anything else for yes. Enter here: ")
-let uppercase = prompt("Would you like to use uppercase letters? Enter 'n' for no, anything else for yes. Enter here: ")
-let numbers = prompt("Would you like to use numbers? Enter 'n' for no, anything else for yes. Enter here: ")
-let special = prompt("Would you like to use special characters? Enter 'n' for no, anything else for yes. Enter here: ")
+createPassword(length);
 
-//Prepare character sets and create password variable
-let passwordCharSet = "";
-if (lowercase != 'n'){
-    passwordCharSet = passwordCharSet.concat(lowerCharSet);
-}
-if (uppercase != 'n'){
-    passwordCharSet = passwordCharSet.concat(upperCharSet);
-}
-if (numbers != 'n'){
-    passwordCharSet = passwordCharSet.concat(numCharSet);
-}
-if (special != 'n'){
-    passwordCharSet = passwordCharSet.concat(specialCharSet);
-}
-console.log(`Possible characters for your password were: ${passwordCharSet}`);
-
-let password = "";
-let nextChar = "";
-let randNum = 0;
-//Loop for desired length
-for(let i = 0; i < length; i++){
-    //Generate secure random value and add to password
-    randNum = crypto.randomInt(passwordCharSet.length);
-    nextChar = passwordCharSet.charAt(randNum);
-    password = password.concat(nextChar);
-}
-
-//return generated password
-console.log(`Your new password is: ${password}. Keep it somewhere safe!`);
